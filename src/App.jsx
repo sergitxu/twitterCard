aa8d6b810ec3f24ee037bf8731680e22c84553b1
sergitxu <sergiortiz@gmail.com>
2024-10-29 23:01:33 +0100

    load data from object

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,42 @@ import "./index.css"
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+
+    const users = [
+        {
+            name: "Sergio Ortiz",
+            userName: "sergitxu",
+            isFollowing: true,
+        },
+        {
+            name: "Miguel Ángel Durán",
+            userName: "midudev",
+            isFollowing: false
+        },
+        {
+            name: "Pablo Márquez",
+            userName: "pableras",
+            isFollowing: true
+        },
+        {
+            name: "Juan Pérez",
+            userName: "juanito  ",
+            isFollowing: false
+        }
+    ];
     return (
         <section className="App">
-            <TwitterFollowCard
-                userName="sergitxu"
-                initialIsFollowing>
-                Sergio Ortiz
-            </TwitterFollowCard>
-            <TwitterFollowCard
-                userName="midudev">
-                Miguel Ángel Durán
-            </TwitterFollowCard>
+            {
+                users.map(({ name, userName, isFollowing }) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </section>
     )
 }
